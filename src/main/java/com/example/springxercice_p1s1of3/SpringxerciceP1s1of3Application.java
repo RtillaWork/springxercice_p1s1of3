e0c321ae66f9b0e2cd9f3ccea7fd44926c7d55df
naominne <39413995+nauranan@users.noreply.github.com>
2021-11-19 16:13:25 -0700

    INITIAL

---
+++ src/main/java/com/example/springxercice_p1s1of3/SpringxerciceP1s1of3Application.java
@@ -0,0 +1,13 @@
+package com.example.springxercice_p1s1of3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringxerciceP1s1of3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringxerciceP1s1of3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springxercice_p1s1of3/SpringxerciceP1s1of3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springxercice_p1s1of3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringxerciceP1s1of3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
