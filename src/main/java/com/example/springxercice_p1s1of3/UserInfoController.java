730a445bac78d5a6449f5f150e34fcab62d58038
naominne <39413995+nauranan@users.noreply.github.com>
2021-11-19 17:12:59 -0700

    add UserInfoController using @RequestBody with @PostMapping

--- src/main/java/com/example/springxercice_p1s1of3/UserInfoController.java
+++ src/main/java/com/example/springxercice_p1s1of3/UserInfoController.java
@@ -1,8 +1,19 @@
 package com.example.springxercice_p1s1of3;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserInfoController {
 
+    @PostMapping("/user")
+    public String userStatus(@RequestBody UserInfo user) {
+        if (user.isEnabled()) {
+            return String.format("Hello %s, account enabled", user.getName());
+        } else {
+            return String.format("Hello %s, account disabled", user.getName());
+        }
+
+    }
 }
