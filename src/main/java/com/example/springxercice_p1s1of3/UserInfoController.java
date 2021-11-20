5ef8672c1f92b840c4d344b82f37a8047b185d90
naominne <39413995+nauranan@users.noreply.github.com>
2021-11-20 04:28:26 -0700

    add List<UserInfo> in both POST and return

--- src/main/java/com/example/springxercice_p1s1of3/UserInfoController.java
+++ src/main/java/com/example/springxercice_p1s1of3/UserInfoController.java
@@ -4,16 +4,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class UserInfoController {
 
     @PostMapping("/user")
-    public String userStatus(@RequestBody UserInfo user) {
-        if (user.isEnabled()) {
-            return String.format("Hello %s, account enabled", user.getName());
+    public String userStatus(@RequestBody UserInfo userInfo) {
+        if (userInfo.isEnabled()) {
+            return String.format("Hello %s, account enabled", userInfo.getName());
         } else {
-            return String.format("Hello %s, account disabled", user.getName());
+            return String.format("Hello %s, account disabled", userInfo.getName());
         }
 
+    }
+
+    @PostMapping("/user/deactivated")
+    public List<UserInfo> userStatus(@RequestBody List<UserInfo> userInfoList) {
+        var enabledUserList = userInfoList.stream()
+                .filter(u -> !u.isEnabled())
+                .collect(Collectors.toUnmodifiableList());
+
+        return enabledUserList;
     }
 }
