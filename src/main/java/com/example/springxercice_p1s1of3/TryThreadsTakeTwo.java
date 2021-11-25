cd15649072e6b804e5daae42d88aba791f58e9da
naominne <39413995+nauranan@users.noreply.github.com>
2021-11-25 14:51:28 -0700

    commit.

--- src/main/java/com/example/springxercice_p1s1of3/TryThreadsTakeTwo.java
+++ src/main/java/com/example/springxercice_p1s1of3/TryThreadsTakeTwo.java
@@ -1,5 +1,14 @@
 package com.example.springxercice_p1s1of3;
 
 public class TryThreadsTakeTwo {
+    class HelloRunnable implements Runnable {
+        @Override
+        public void run() {
+            String threadName = Thread.currentThread().getName();
+            String hello = String.format("Hello, my name is %s", threadName);
+            System.out.println(hello);
+        }
+
+    }
 
 }
